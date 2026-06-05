9587feb1ea733245f7d528e83adf587aac60e293
Anubhav <xcaliber704@gmail.com>
2026-06-06 01:37:45 +0530

    testing cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
       res.cookie("token", token, {
          httpOnly: true,
          secure: true,
-         sameSite: "strict",
+         sameSite: "none",
          maxAge: 5 * 60 * 60 * 1000
       })
 
@@ -83,7 +83,7 @@ export const login = async (req, res) => {
       res.cookie("token", token, {
          httpOnly: true,
          secure: true,
-         sameSite: "strict",
+         sameSite: "none",
          maxAge: 5 * 60 * 60 * 1000
       })
 
@@ -100,7 +100,7 @@ export const logout = (req, res) => {
    res.clearCookie("token", {
       httpOnly: true,
       secure: true,
-      sameSite: "strict"
+      sameSite: "none"
    })
 
    return res.status(200).json({
